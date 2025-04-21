0f63cc4a0f42bccf4cd09c8bc135a89752a202ba
Jason Frame <jwf@jasonframe.co.uk>
2025-04-21 17:14:45 +0100

    tidy up dev manifest

--- src/dev/Dev.php
+++ src/dev/Dev.php
@@ -7,17 +7,17 @@ class Dev {
         if (!is_dir($devDir)) {
             mkdir($devDir, 0755, true);
         }
-        
-        if (!file_exists("$devDir/supervisord.conf")) {
-            copy(__DIR__ . "/supervisord.conf", "$devDir/supervisord.conf");
-        }
 
-        if (!file_exists("$devDir/nginx.conf")) {
-            copy(__DIR__ . "/nginx.conf", "$devDir/nginx.conf");
-        }
+        $manifest = [
+            "supervisord.conf",
+            "nginx.conf",
+            "Dockerfile"
+        ];
 
-        if (!file_exists("$devDir/Dockerfile")) {
-            copy(__DIR__ . "/Dockerfile", "$devDir/Dockerfile");
-        } 
+        foreach ($manifest as $f) {
+            if (!file_exists("$devDir/$f")) {
+                copy(__DIR__ . "/$f", "$devDir/$f");
+            }
+        }
     }
 }
